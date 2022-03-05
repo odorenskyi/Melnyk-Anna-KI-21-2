e6c1d0f774ace8b9c3e19ff1c923dfa60c5c5db2
MelnikAnna <91954998+Anna122233234@users.noreply.github.com>
2022-03-05 21:59:36 +0200

    add task1 and ModulesMelnik folder

---
+++ lab08/prj/Melnik_task/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+#include "ModulesMelnik.h"
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    int x = 0, z=0;
+    cout << "Enter x" << endl;
+    cin >> x;
+    cout << "Enter z" << endl;
+    cin >> z;
+    cout << s_calculation( x,z ) << endl;
+}
---
+++ lab08/prj/prj/ModulesMelnik/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+#include <tgmath.h>
+#include <windows.h>
+#include <string>
+
+using namespace std;
+
+float s_calculation(int x, int z)
+{
+    const double pi = 3.14159;
+    return (sqrt ( 1 + abs ( cos (x))) +(2 * pi)+ (pow(abs(z-x) / sin (x), 2)));
+}
