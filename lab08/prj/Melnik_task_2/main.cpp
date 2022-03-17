d6fb34ac4088178935f06bc1422b87794a78c37a
MelnikAnna <91954998+Anna122233234@users.noreply.github.com>
2022-03-17 09:37:42 +0200

    add changed task2

--- lab08/prj/Melnik_task_2/main.cpp
+++ lab08/prj/Melnik_task_2/main.cpp
@@ -3,10 +3,49 @@
 
 using namespace std;
 
+void copyright()
+{
+    cout << "© ћельник јнна" << endl << endl;
+}
+void res ()
+{
+    int  a = 0,b = 0;
+    if (a + 3 <= b){
+        cout <<"False"<<endl;
+    }
+    else {
+        cout <<"True"<<endl;
+    }
+
+}
+void DecAndHex(int x, int y, int z){
+    cout << "'y' в дес€тков≥й: " << x << endl
+         << "'x' в дес€тков≥й: " << y << endl
+         << "'z' в дес€тков≥й: " << z << endl << endl;
+
+    cout << "'y' в ш≥стнац€тков≥й: " << hex << x << endl
+         << "'x' в ш≥стнац€тков≥й: " << hex << y << endl
+         << "'z' в ш≥стнац€тков≥й: " << hex << z << endl;
+}
+
 int main()
 {
-    int x=9;
-    int y=2;
-    cout << s_calculation(x,y);
+    system("chcp 1251 && cls");
+    copyright();
+    int x = 0,y = 0,z = 0;
+    int  a = 0,b = 0;
+    cout << "¬вед≥ть x" << endl;
+    cin >> x;
+    cout << "¬вед≥ть y" << endl;
+    cin >> y;
+    cout << "¬вед≥ть z" << endl;
+    cin >> z;
+    cout << "¬вед≥ть a" << endl;
+    cin >> a ;
+    cout << "¬вед≥ть b" << endl;
+    cin >> b;
+    cout <<"S= "<< s_calculation(x,y)<< endl;
+    res();
+    DecAndHex(x,y,z);
     return 0;
 }
