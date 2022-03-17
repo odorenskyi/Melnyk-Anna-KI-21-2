73353d7d19162f16074dd4167dfd2c7186f2b4d6
MelnikAnna <91954998+Anna122233234@users.noreply.github.com>
2022-03-17 07:29:39 +0200

    add task1

--- lab08/prj/Melnik_task_1/main.cpp
+++ lab08/prj/Melnik_task_1/main.cpp
@@ -8,10 +8,10 @@ using namespace std;
 int main()
 {
     system("chcp 1251 && cls");
-    int x = 0, y=0, z=0, a=0, b=0;
+    int x = 0,z=0;
     cout << "¬вед≥ть x" << endl;
     cin >> x;
     cout << "¬вед≥ть z" << endl;
     cin >> z;
-    cout << s_calculation(x,z) << endl;
+    cout <<"S= " << s_calculation(x,z) << endl;
 }
