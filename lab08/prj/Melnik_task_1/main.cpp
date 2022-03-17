b4b5d9f458c2fb238e241356b84e8e6c38b5fd5c
MelnikAnna <91954998+Anna122233234@users.noreply.github.com>
2022-03-17 07:16:58 +0200

    add task2

--- lab08/prj/Melnik_task_1/main.cpp
+++ lab08/prj/Melnik_task_1/main.cpp
@@ -5,28 +5,13 @@
 
 using namespace std;
 
-void copyright()
-{
-    system("chcp 1251 && cls");
-    cout << "© Мельник Анна" << endl << endl;
-}
-void calculus(){
-    int x = 0,  y=0, z=0;
-    cout << "'x' В десятковій: " << dec << x
-         << "\t'x' В шістнадцятковій: " << hex << x << endl;
-    cout << "'y' В десятковій: " << dec << y
-         << "\t'y' В шістнадцятковій: " << hex << y << endl;
-    cout << "'z' В десятковій: " << dec << z
-         << "\t'z' В шістнадцятковій: " << hex << z << endl << endl;
-}
 int main()
 {
-    copyright();
+    system("chcp 1251 && cls");
     int x = 0, y=0, z=0, a=0, b=0;
     cout << "Введіть x" << endl;
     cin >> x;
     cout << "Введіть z" << endl;
     cin >> z;
-    cout << s_calculation( x,z ) << endl;\
-    calculus();
+    cout << s_calculation(x,z) << endl;
 }
---
+++ lab08/prj/Melnik_task_2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "ModulesMelnik.h"
+
+using namespace std;
+
+int main()
+{
+    int x=9;
+    int y=2;
+    cout << s_calculation(x,y);
+    return 0;
+}
